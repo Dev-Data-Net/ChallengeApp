eb7914e70f367b1b7509963c584568dcaaed203d
Dev-Data-Net <dev.data.net@gmail.com>
2024-04-28 15:29:50 +0200

    Day 4 - variables

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,1 +1,31 @@
-﻿Console.WriteLine("Hello Dawid, again!");
+﻿// liczby całkowite
+var myAge = 33;
+int myAge2 = 120;
+int newAge = myAge2 + 100;
+Console.WriteLine(newAge);
+
+int myAge3 = int.MinValue;
+uint myAge4 = uint.MaxValue;
+long myAge5 = long.MaxValue;
+ulong myAge6 = ulong.MaxValue;
+
+// liczby zmiennoprzecinkowe
+float myNumber = float.MaxValue;
+double myNumber2 = double.MaxValue;
+decimal myNumber3 = decimal.MaxValue;
+
+// zmienne tekstowe
+string name = "Jan";
+string surname = "Kowalski";
+string result = name + surname + newAge;
+Console.WriteLine(result);
+char myVar = 'c';
+var result2 = name.ToArray();
+
+// zmienne logiczne
+bool isActive = true;
+bool isActive2 = false;
+var isValid = 5 > 6;
+Console.WriteLine(isValid);
+
+
