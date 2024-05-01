a1d32d29b630372f581bddb2397e0cf6c55a12c0
Dev-Data-Net <dev.data.net@gmail.com>
2024-05-01 22:05:57 +0200

    Homework day 5

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,25 +1,75 @@
-﻿// tablica
-int[] grades = new int[365];
+﻿// zadanie domowe dzień 5
 
-// lista
-List<string> dayOfWeeks = new List<string>();
-dayOfWeeks.Add("poniedziałek");
-dayOfWeeks.Add("wtorek");
-dayOfWeeks.Add("środa");
-dayOfWeeks.Add("czwartek");
-dayOfWeeks.Add("piątek");
-dayOfWeeks.Add("sobota");
-dayOfWeeks.Add("niedziela");
-//Console.WriteLine(dayOfWeeks[2]);
+Console.WriteLine("Podaj dowolną liczbę.");
+string number = Console.ReadLine();
+char[] letters = number.ToArray();
 
 
+int counter0 = 0;
+int counter1 = 0;
+int counter2 = 0;
+int counter3 = 0;
+int counter4 = 0;
+int counter5 = 0;
+int counter6 = 0;
+int counter7 = 0;
+int counter8 = 0;
+int counter9 = 0;
 
-// pętla "foreach"
-foreach (var day in dayOfWeeks)
+foreach (char num in letters)
 {
-    Console.WriteLine(day);
+    if (num == '0')
+    {
+        counter0++;
+    }
+    else if (num == '1')
+    {
+        counter1++;
+    }
+    else if (num == '2')
+    {
+        counter2++;
+    }
+    else if (num == '3')
+    {
+        counter3++;
+    }
+    else if (num == '4')
+    {
+        counter4++;
+    }
+    else if (num == '5')
+    {
+        counter5++;
+    }
+    else if (num == '6')
+    {
+        counter6++;
+    }
+    else if (num == '7')
+    {
+        counter7++;
+    }
+    else if (num == '8')
+    {
+        counter8++;
+    }
+    else if (num == '9')
+    {
+        counter9++;
+    }
 }
-
-
+Console.WriteLine();
+Console.WriteLine("W liczbie: " + number);
+Console.WriteLine("Liczb 0 jest: " + counter0);
+Console.WriteLine("Liczb 1 jest: " + counter1);
+Console.WriteLine("Liczb 2 jest: " + counter2);
+Console.WriteLine("Liczb 3 jest: " + counter3);
+Console.WriteLine("Liczb 4 jest: " + counter4);
+Console.WriteLine("Liczb 5 jest: " + counter5);
+Console.WriteLine("Liczb 6 jest: " + counter6);
+Console.WriteLine("Liczb 7 jest: " + counter7);
+Console.WriteLine("Liczb 8 jest: " + counter8);
+Console.WriteLine("Liczb 9 jest: " + counter9);
 
 
