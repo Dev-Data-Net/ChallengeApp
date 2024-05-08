ad3702b298f96f3497752672e9d9a792ccf109c5
Dev-Data-Net <dev.data.net@gmail.com>
2024-05-08 17:51:17 +0200

    Day 6 - classes in files

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,4 +1,6 @@
-﻿User user1 = new User("Stanisław", "3434fdsefd");
+﻿using ChallengeApp;
+
+User user1 = new User("Stanisław", "3434fdsefd");
 User user2 = new User("Mieczysław", "f342dfdsff");
 User user3 = new User("Mirosław", "dsfsarw34344");
 User user4 = new User("Lesław", "dsfsfsfsdf34344");
@@ -7,39 +9,6 @@ user1.AddScore(5);
 user1.AddScore(2);
 var result = user1.Result;
 Console.WriteLine(result);
-var name = User.GameName;
-var pi = Math.PI;
-
-class User
-{
-    // zmienna statyczna
-    public static string GameName = "Diablo";
-
-    private List<int> score = new List<int>();
-
-    // konstruktor
-    public User(string login, string password)
-    {
-        this.Login = login;
-        this.Password = password;
-    }
-
-    // pola w klasach
-    public string Login { get; private set; }
-    public string Password { get; private set; }
-
-    public int Result
-    {
-        get
-        {
-            return this.score.Sum();
-        }
-    }
 
 
-    //metody
-    public void AddScore(int score)
-    {
-        this.score.Add(score);
-    }
-}
+
---
+++ ChallengeApp/ChallengeApp/User.cs
@@ -0,0 +1,35 @@
+﻿namespace ChallengeApp
+{
+    public class User
+    { // zmienna statyczna
+        public static string GameName = "Diablo";
+
+        private List<int> score = new List<int>();
+
+        // konstruktor
+        public User(string login, string password)
+        {
+            this.Login = login;
+            this.Password = password;
+        }
+
+        // pola w klasach
+        public string Login { get; private set; }
+        public string Password { get; private set; }
+
+        public int Result
+        {
+            get
+            {
+                return this.score.Sum();
+            }
+        }
+
+
+        //metody
+        public void AddScore(int score)
+        {
+            this.score.Add(score);
+        }
+    }
+}
