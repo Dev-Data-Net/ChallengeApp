7b8264d6df2ae8c65843e8d70221df618f2a94ee
Dev-Data-Net <dev.data.net@gmail.com>
2024-05-08 16:59:21 +0200

    Day 6 - methods

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -3,19 +3,28 @@ User user2 = new User("Mieczysław", "f342dfdsff");
 User user3 = new User("Mirosław", "dsfsarw34344");
 User user4 = new User("Lesław", "dsfsfsfsdf34344");
 
-var name = user1.Login;
+user1.AddScore(5);
 
 class User
 {
+    private int score;
 
-
+    // konstruktor
     public User(string login, string password)
     {
         this.Login = login;
         this.Password = password;
+        this.score = 0;
     }
 
     // pola w klasach
     public string Login { get; private set; }
     public string Password { get; private set; }
+
+
+    //metody
+    public void AddScore(int score)
+    {
+        this.score += score;
+    }
 }
