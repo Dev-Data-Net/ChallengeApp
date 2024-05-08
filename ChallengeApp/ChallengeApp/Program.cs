e02ff553e812b85601849cbf10ad04b668edad21
Dev-Data-Net <dev.data.net@gmail.com>
2024-05-08 17:40:03 +0200

    Day 6 - methods and hermetization

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -4,27 +4,37 @@ User user3 = new User("Mirosław", "dsfsarw34344");
 User user4 = new User("Lesław", "dsfsfsfsdf34344");
 
 user1.AddScore(5);
+user1.AddScore(2);
+var result = user1.Result;
+Console.WriteLine(result);
 
 class User
 {
-    private int score;
+    private List<int> score = new List<int>();
 
     // konstruktor
     public User(string login, string password)
     {
         this.Login = login;
         this.Password = password;
-        this.score = 0;
     }
 
     // pola w klasach
     public string Login { get; private set; }
     public string Password { get; private set; }
 
+    public int Result
+    {
+        get
+        {
+            return this.score.Sum();
+        }
+    }
+
 
     //metody
     public void AddScore(int score)
     {
-        this.score += score;
+        this.score.Add(score);
     }
 }
