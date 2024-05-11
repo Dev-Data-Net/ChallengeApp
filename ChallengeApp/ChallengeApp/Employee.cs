eb0718df72570c356ad3feb21ec3c72926726051
Dev-Data-Net <dev.data.net@gmail.com>
2024-05-11 22:33:38 +0200

    Day 9 - preparation of the data model

---
+++ ChallengeApp/ChallengeApp/Employee.cs
@@ -0,0 +1,21 @@
+﻿namespace ChallengeApp
+{
+    public class Employee
+    {
+        private List<float> grades = new List<float>();
+
+        public Employee(string name, string surname)
+        {
+            this.Name = name;
+            this.Surname = surname;
+        }
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+
+        public void AddGrade(float grade)
+        {
+            this.grades.Add(grade);
+        }
+
+    }
+}
