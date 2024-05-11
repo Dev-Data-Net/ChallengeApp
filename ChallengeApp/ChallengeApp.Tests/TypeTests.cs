a1a36b6b59b23bae5a2e8631a94dacf563c92507
Dev-Data-Net <dev.data.net@gmail.com>
2024-05-11 13:39:30 +0200

    Day 8 - homework

--- ChallengeApp/ChallengeApp.Tests/TypeTests.cs
+++ ChallengeApp/ChallengeApp.Tests/TypeTests.cs
@@ -1,15 +1,14 @@
-﻿
-namespace ChallengeApp.Tests
+﻿namespace ChallengeApp.Tests
 {
     public class TypeTests
     {
         // typ wartościowy
         [Test]
-        public void TEST()
+        public void ValueTestTypeString()
         {
             // arrange
             string number1 = "1";
-            string number2 = "13";
+            string number2 = "1";
 
             // act
 
@@ -20,13 +19,45 @@ namespace ChallengeApp.Tests
 
         }
 
-        // typ referencyjny
         [Test]
-        public void DifferentObjects()
+        public void ValueTestTypeInt()
         {
             // arrange
-            var user1 = GetUser("Adam");
-            var user2 = GetUser("Adam");
+            int number1 = 1;
+            int number2 = 1;
+
+            // act
+
+
+            // assert
+            Assert.AreEqual(number1, number2);
+
+
+        }
+
+        [Test]
+        public void ValueTestTypeFloat()
+        {
+            // arrange
+            float number3 = 1.50f;
+            float number4 = 1.50f;
+
+            // act
+
+
+            // assert
+            Assert.AreEqual(number3, number4);
+
+
+        }
+
+        // typ referencyjny
+        [Test]
+        public void ReferenceTestTypeDifferentObjects()
+        {
+            // arrange
+            var user1 = GetUser("Zbysław");
+            var user2 = GetUser("Zbysław");
 
             // act
 
@@ -36,7 +67,20 @@ namespace ChallengeApp.Tests
 
 
         }
+        public void ReferenceTestTypeSameObjects()
+        {
+            // arrange
+            var user1 = GetUser("Adam");
+            var user2 = GetUser("Adam");
 
+            // act
+
+
+            // assert
+            Assert.AreNotEqual(user1.Login, user2.Login);
+
+
+        }
         private User GetUser(string name)
         {
             return new User(name);
