d181d116b5d5927a420b8417a22f96af3d6e67bf
Dev-Data-Net <dev.data.net@gmail.com>
2024-05-15 23:26:52 +0200

    Day 9 - implementing a statistics model using Math

--- ChallengeApp/ChallengeApp/Employee.cs
+++ ChallengeApp/ChallengeApp/Employee.cs
@@ -20,7 +20,18 @@
         public Statistics GetStatistics()
         {
             var statistics = new Statistics();
+            statistics.Average = 0;
+            statistics.Max = float.MinValue;
+            statistics.Min = float.MaxValue;
 
+            foreach (var grade in this.grades)
+            {
+                statistics.Max = Math.Max(statistics.Max, grade);
+                statistics.Min = Math.Min(statistics.Min, grade);
+                statistics.Average += grade;
+            }
+
+            statistics.Average = statistics.Average / this.grades.Count;
             return statistics;
         }
 
