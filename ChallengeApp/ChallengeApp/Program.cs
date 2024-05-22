d14ccf3a591bf2813348cb583654c6353bd7d952
Dev-Data-Net <dev.data.net@gmail.com>
2024-05-22 21:45:39 +0200

    Day 11 - homework

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,12 +1,41 @@
 ﻿using ChallengeApp;
 
 var employee = new Employee("Jan", "Kowalski");
-employee.AddGrade("Adam");
-employee.AddGrade("4000");
+employee.AddGrade(1);
+employee.AddGrade(8);
 employee.AddGrade(2);
 employee.AddGrade(6);
 var statistics = employee.GetStatistics();
-Console.WriteLine($"Average: {statistics.Average:N2}"); //:N2 - float do dwóch miejsc po przecinku
+var statistics1 = employee.GetStatisticsWithForEach();
+var statistics2 = employee.GetStatisticsWithFor();
+var statistics3 = employee.GetStatisticsWithDoWhile();
+var statistics4 = employee.GetStatisticsWithWhile();
+
+Console.WriteLine("Get Statistics:");
+Console.WriteLine($"Average: {statistics.Average:N2}");
 Console.WriteLine($"Min: {statistics.Min}");
 Console.WriteLine($"Max: {statistics.Max}");
+Console.WriteLine();
 
+Console.WriteLine("Get Statistics With For Each:");
+Console.WriteLine($"Average: {statistics1.Average:N2}");
+Console.WriteLine($"Min: {statistics1.Min}");
+Console.WriteLine($"Max: {statistics1.Max}");
+Console.WriteLine();
+
+Console.WriteLine("Get Statistics With For:");
+Console.WriteLine($"Average: {statistics2.Average:N2}");
+Console.WriteLine($"Min: {statistics2.Min}");
+Console.WriteLine($"Max: {statistics2.Max}");
+Console.WriteLine();
+
+Console.WriteLine("Get Statistics With Do While:");
+Console.WriteLine($"Average: {statistics3.Average:N2}");
+Console.WriteLine($"Min: {statistics3.Min}");
+Console.WriteLine($"Max: {statistics3.Max}");
+Console.WriteLine();
+
+Console.WriteLine("Get Statistics With While:");
+Console.WriteLine($"Average: {statistics4.Average:N2}");
+Console.WriteLine($"Min: {statistics4.Min}");
+Console.WriteLine($"Max: {statistics4.Max}");
