9917c6e62f300fa12d5841f5c1bc96d60c6cf926
Dev-Data-Net <dev.data.net@gmail.com>
2024-05-24 22:26:31 +0200

    Day 12 - controlling communication with the user

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -3,10 +3,22 @@
 Console.WriteLine("Witamy w programie ocen pracowników");
 Console.WriteLine("===================================");
 Console.WriteLine();
-Console.WriteLine("Podaj ocenę pracownika: ");
-var input = Console.ReadLine();
 
 var employee = new Employee();
-employee.AddGrade(input);
+
+while (true)
+{
+    Console.WriteLine("Podaj kolejną ocenę pracownika: ");
+    Console.WriteLine("Jeśli chcesz zakończyć naciśnij Q.");
+    var input = Console.ReadLine();
+    if (input == "Q" || input == "q")
+    {
+        break;
+    }
+    employee.AddGrade(input);
+}
+
 var statistics = employee.GetStatistics();
-Console.WriteLine(statistics.Average);
+Console.WriteLine($"Średnia ocen: {statistics.Average}");
+Console.WriteLine($"Najniższa ocena: {statistics.Min}");
+Console.WriteLine($"Najwyższa ocena: {statistics.Max}");
