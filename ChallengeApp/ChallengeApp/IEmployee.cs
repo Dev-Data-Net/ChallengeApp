65868a0980e21b3c5e079649439ef7e5de1815e8
Dev-Data-Net <dev.data.net@gmail.com>
2024-06-10 00:13:10 +0200

    Day 15 - working with interfaces

--- ChallengeApp/ChallengeApp/IEmployee.cs
+++ ChallengeApp/ChallengeApp/IEmployee.cs
@@ -5,6 +5,15 @@
         string Name { get; }
 
         string Surname { get; }
+
+        void AddGrade(float grade);
+
+        void AddGrade(double grade);
+
+        void AddGrade(int grade);
+
+        void AddGrade(char grade);
+
         Statistics GetStatistics();
     }
 }
--- ChallengeApp/ChallengeApp/Supervisor.cs
+++ ChallengeApp/ChallengeApp/Supervisor.cs
@@ -6,6 +6,26 @@
 
         public string Surname => "Kowalski";
 
+        public void AddGrade(float grade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddGrade(double grade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddGrade(int grade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddGrade(char grade)
+        {
+            throw new NotImplementedException();
+        }
+
         public Statistics GetStatistics()
         {
             return new Statistics();
